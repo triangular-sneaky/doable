91a36ce24e09e0cf2f2482a96df4a61515512927
DPLV\dpoz <dpoznyak@gmail.com>
2019-01-27 14:34:59 +0800

    stop via meteor....bat

--- meteor@docker-dev.bat
+++ meteor@docker-dev.bat
@@ -1,1 +1,4 @@
-docker-compose -f .\docker-compose-debug.yml run --service-ports --rm wekan
+@echo off
+
+docker-compose -f .\docker-compose-debug.yml down
+if x%1 NEQ xstop docker-compose -f .\docker-compose-debug.yml run --service-ports --rm wekan
